35be3d218b41817e4c72fd3d2e3fc6b3fa2e3786
Nurjahon <166979887+n20090920@users.noreply.github.com>
2024-07-22 14:17:27 +0500

    crud qilindi

--- src/pages/Teacher.jsx
+++ src/pages/Teacher.jsx
@@ -1,46 +1,74 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dashboard from "../components/Dashboard";
-import { Button } from "@mui/material";
+import "../App.css";
+import {
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  TextField,
+} from "@mui/material";
+import axios from "axios";
 
 const Teacher = () => {
-  const [items, setItems] = useState([
-    { id: 1, name: "Item 1" },
-    { id: 2, name: "Item 2" },
-    { id: 3, name: "Item 3" },
-  ]);
+  const [items, setItems] = useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [level, setLevel] = useState("");
 
-  const addItem = (name) => {
-    const newItem = { id: items.length + 1, name };
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3000/teachers`)
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data!", error);
+      });
+  }, []);
+
+  const addItem = (firstName, lastName, level) => {
+    const newItem = { id: items.length + 1, firstName, lastName, level };
     setItems([...items, newItem]);
   };
-  const displayItems = items.map((item) => (
-    <div key={item.id}>
-      <span>{item.name}</span>
-      <button onClick={() => updateItem(item.id)}>Edit</button>
-      <button onClick={() => deleteItem(item.id)}>Delete</button>
-    </div>
-  ));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && lastName && level) {
+      addItem(firstName, lastName, level);
+      setFirstName("");
+      setLastName("");
+      setLevel("");
+    }
+  };
+
   const updateItem = (id) => {
-    const updatedName = prompt("Enter the new name:");
-    if (updatedName) {
+    const updatedFirstName = prompt("Enter the new firstName:");
+    const updatedLastName = prompt("Enter the new lastName:");
+    const updatedLevel = prompt("Enter the new level:");
+    if (updatedFirstName && updatedLastName && updatedLevel) {
       const updatedItems = items.map((item) =>
-        item.id === id ? { ...item, name: updatedName } : item
+        item.id === id
+          ? {
+              ...item,
+              firstName: updatedFirstName,
+              lastName: updatedLastName,
+              level: updatedLevel,
+            }
+          : item
       );
       setItems(updatedItems);
     }
   };
+
   const deleteItem = (id) => {
     const updatedItems = items.filter((item) => item.id !== id);
     setItems(updatedItems);
   };
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const etiv = e.target.itemName.value;
-    if (!etiv == "") {
-      addItem(etiv);
-      etiv = "";
-    }
-  };
+
   return (
     <div style={{ display: "flex" }} className="container">
       <Dashboard />
@@ -52,13 +80,66 @@ const Teacher = () => {
           fontSize: "22px",
           gap: "15px",
         }}>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="itemName" placeholder="Enter item name" />
-          <Button variant="contained" type="submit">
+        <form onSubmit={handleSubmit} className="fetch_form">
+          <TextField
+            variant="outlined"
+            type="text"
+            name="firstName"
+            label="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+            required
+          />
+          <TextField
+            variant="outlined"
+            type="text"
+            name="lastName"
+            label="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+            required
+          />
+          <TextField
+            variant="outlined"
+            type="text"
+            name="level"
+            label="Level"
+            value={level}
+            onChange={(e) => setLevel(e.target.value)}
+            required
+          />
+          <Button variant="contained" type="submit" sx={{ height: "53px" }}>
             Add Item
           </Button>
         </form>
-        {displayItems}
+        {/* <TableContainer> */}
+        <Table sx={{ marginLeft: "0", marginTop: "0" }} className="Table">
+          <TableHead>
+            <TableRow>
+              <TableCell>First Name</TableCell>
+              <TableCell>Last Name</TableCell>
+              <TableCell>Level</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {items.map((item) => (
+              <TableRow
+                key={item.id}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                <TableCell>{item.firstName}</TableCell>
+                <TableCell>{item.lastName}</TableCell>
+                <TableCell>{item.level}</TableCell>
+                <Button variant="contained" onClick={() => updateItem(item.id)}>
+                  Edit
+                </Button>
+                <Button variant="contained" onClick={() => deleteItem(item.id)}>
+                  Delete
+                </Button>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        {/* </TableContainer> */}
       </div>
     </div>
   );
